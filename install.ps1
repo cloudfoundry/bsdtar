ceb5b0c4ffe69745d6b55421aa2957e6c92b814d
Natalie Arellano <narellano@pivotal.io>
2017-03-09 10:42:56 -0500

    Create output directory
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- install.ps1
+++ install.ps1
@@ -101,4 +101,5 @@ $tarball = ("tar-{0}.exe" -f ([int][double]::Parse((Get-Date -UFormat %s))))
 $outputTar = [System.IO.Path]::Combine("tar-output", $tarball)
 
 # Capture the output in Concourse
+New-Item -type Directory "tar-output" -Force
 Move-Item $bsdTar $outputTar
